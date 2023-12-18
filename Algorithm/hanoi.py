cccaa6170b3ee15071ded912089ed2d7e5465e00
Mike <706420258@qq.com>
2023-12-18 18:27:39 +0800

    init

--- Algorithm/hanoi.py
+++
@@ -1,6 +0,0 @@
-def hanoi(n:int,a:str,b:str,c:str):
-    if n>0:
-        hanoi(n-1,a,c,b)
-        print("moving from %s to %s" %(a,c))
-        hanoi(n-1,b,a,c)
-hanoi(3,"A","B","C")
--- Algorithm/liner_search.py
+++
@@ -1,8 +0,0 @@
-def liner_search(list:list,val):
-    for index,v in enumerate(list):
-        if v==val:
-            return index
-    else:
-        return None
-        
-print(liner_search([1,2,3],2))
