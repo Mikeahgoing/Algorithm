30315361a3b7f710696700b2d3e59b3ee2253616
Mike <706420258@qq.com>
2023-12-18 20:12:46 +0800

    2023-12-18 Q3

--- question3.js
+++ question3.js
@@ -3,5 +3,14 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+    let i = 0;
+    while (i < nums.length) {
+      if (nums[i] === nums[i + 1]) {
+        nums.splice(i + 1, 1);
+      } else {
+        i++;
+      }
+    }
+    return nums.length;
+  };
 
-};
