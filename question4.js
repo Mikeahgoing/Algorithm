0299c756a9a15cdd1657e484ade23565be292652
Mike <706420258@qq.com>
2023-12-19 17:37:07 +0800

    2023-12-19 Q4

---
+++ question4.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  if (nums.length <= 2) {
+    return nums.length;
+  }
+  let slow = 2;
+  let fast = 2;
+  while (fast < nums.length) {
+    if (nums[fast] !== nums[slow - 2]) {
+      nums[slow] = nums[fast];
+      slow++;
+    }
+    fast++;
+  }
+  return slow
+};
